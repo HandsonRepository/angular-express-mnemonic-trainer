af15a494e991d8b7a8293d73b0f32bc98a65f678
Johndoe <56298669+togane@users.noreply.github.com>
2023-01-28 16:04:55 +0900

    🧪 openapi で適当なエンドポイント1つ作ってみた

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,24 +1,33 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client'
+import express, { Express, Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { initialize } from 'express-openapi';
+import { getWords } from './api/operations/word';
+import ApiDoc from './api/openapi.json';
 
-const app: express.Express = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app: Express = express();
 
-// ローカルでしか動かさないため CROS はノーガード
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-})
+const corsOptions = {
+  origin: 'https://editor.swagger.io',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+initialize({
+  app,
+  // todo: できれば yml で定義したい
+  apiDoc: {
+    ...JSON.parse(JSON.stringify(ApiDoc)),
+  },
+  validateApiDoc: false,
+  operations: {
+    getWords,
+  },
+  errorMiddleware: (err, req: Request, res: Response, _next: NextFunction) => {
+    console.error(err);
+    res.status(err.status || 500).json(err);
+  }
+});
 
 app.listen(3000, () => {
   console.log("Start on port 3000.");
-})
-
-const prisma = new PrismaClient()
-
-app.get('/words', async (req: express.Request, res: express.Response) => {
-  res.send(JSON.stringify(await prisma.words.findMany({ take: 10 })));
 })
